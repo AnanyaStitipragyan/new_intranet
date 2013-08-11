9bb1b3c274d6e70eb07fe9c7c643ac787a805f44
liveashish <ashish.sahoo@gmail.com>
2013-08-12 01:41:26 +0530

    URL linking with intranet fixed.

--- index.php
+++ index.php
@@ -23,6 +23,7 @@ $target_url = 'http://hib.ximb.ac.in/Hibiscus/Pub/nbDocList.php?client=iiit';
 $html = new simple_html_dom();
 $html->load_file($target_url);
 echo '<div class="container">';
+$url='http://hib.ximb.ac.in/Hibiscus/Pub/';
 foreach($html->find("tr[class=LOV]") as $post){
 
 $author=$post->find("td[width=20%]",0)->find("font[color=red]",0)->innertext;
@@ -30,7 +31,12 @@ $post->find("td[width=20%]",0)->find("font[color=red]",0)->innertext='';
 $post->find("font[color=blue]",0)->color='#8D8D8D';
 $post->find("font[color=blue]",0)->outertext='';
 $post->find("td[width=80]",0)->outertext='';
+$url_2=$post->find("a[rel=prettyPhoto[pp_gal]]",0)->href;
+$link_url= $url.''. $url_2;
+$post->find("a[rel=prettyPhoto[pp_gal]]",0)->href=$link_url;
+$post->find("a[rel=prettyPhoto[pp_gal]]",0)->target='_blank';
 $post->find("a[href=*]",0)->outertext='';
+
 
 
 echo '<section class="main">
@@ -40,7 +46,8 @@ echo '<section class="main">
 						<p><a href="http://hib.ximb.ac.in/Hibiscus/Pub/"'.$post.'</p>
 					</div>
 					<div class="mb-attribution">
-						<p class="mb-author">'.$author.'</p>
+					<a href="http://hib.ximb.ac.in/Hibiscus/Pub/"
+						<p class="mb-author">'.$author.'</p></a>
 						<div class="mb-thumb"></div>
 					</div>
 				</div>
